f8d18f17ecece7c86dbf01e07acd337048e40620
Gheorghe Lisca <liscagheorghe@gmail.com>
2019-05-21 12:20:41 +0200

    run file for classification.

---
+++ run_classification.py
@@ -0,0 +1,11 @@
+import classification as cls
+
+def run_classification():
+    reload(cls)
+    cls.training_file_name = './data/classification_data.txt'
+    cls.groundtruth_file_name = './data/classification_groundtruth.txt'
+    cls.run()
+
+if __name__ == '__main__':
+    run_classification()
+
