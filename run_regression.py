16496377774d02c4a6b2bc0c4567e6286620010c
Gheorghe Lisca <liscagheorghe@gmail.com>
2019-05-21 12:20:52 +0200

    run file for regression.

---
+++ run_regression.py
@@ -0,0 +1,10 @@
+import regression as reg
+
+def run_regression():
+    reload(reg)
+    reg.training_file_name = './data/regression_data.txt'
+    reg.groundtruth_file_name = './data/regression_groundtruth.txt'
+    reg.run()
+
+if __name__ == '__main__':
+    run_regression()
